70a15e12c18709bcfcf6fa5e6a169e24b564c417
Sidibe <Elstevehcks@gmail.com>
2024-03-22 14:02:25 -0400

    Add inital express app

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 import bodyParser from "body-parser";
 import express,  { query } from "express";
-import { pg } from "pg";
+import pg  from "pg";
 import { dirname } from "path";
 import ejs from "ejs";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const db = new pg.Client({
+    host: 'localhost',
+    port: '5433',
+    database: 'ehotels',
+    user: 'postgres',
+    password: '8@9eqtd5n4cBTED!'
+});
+db.connect();
+const port = 3000;
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.listen(port, (req, res) => {
+    console.log(`Listening on port : ${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.render('hotels-list.ejs');
+});
