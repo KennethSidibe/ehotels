3a77b3a02e9232102a2a7b5d13a21e02e6f9e5c9
Sidibe <Elstevehcks@gmail.com>
2024-03-22 18:19:17 -0400

    Add code to test index methods

---
+++ test.js
@@ -0,0 +1,21 @@
+// test index.js functions
+
+/*
+let testExtract = extractCityCountry('101 coulés des Kouadio, Ouagadougou, Burkina Faso, Q2E 2R2');
+console.log(`Test Extract func: ${testExtract}`);
+let testCategoryToIcon = categoryToIcon('relaxation');
+let testCategoryToIcon2 = categoryToIcon('luxe');
+let testCategoryToIcon3 = categoryToIcon('éco');
+let testCategoryToIcon4 = categoryToIcon('travail');
+let testCategoryToIcon5 = categoryToIcon('royal');
+let testCategoryToIcon6 = categoryToIcon('vacances');
+let testCategoryToIcon7 = categoryToIcon('palabres');
+console.log(`Test Category func 1: ${testCategoryToIcon}`);
+console.log(`Test Category func 2: ${testCategoryToIcon2}`);
+console.log(`Test Category func 3: ${testCategoryToIcon3}`);
+console.log(`Test Category func 4: ${testCategoryToIcon4}`);
+console.log(`Test Category func 5: ${testCategoryToIcon5}`);
+console.log(`Test Category func 6: ${testCategoryToIcon6}`);
+console.log(`Test Category func 7: ${testCategoryToIcon7}`);
+
+*/
