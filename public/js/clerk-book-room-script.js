7fc164b56cd314bf09bf1100b0e8ba41bdabbdef
Sidibe <Elstevehcks@gmail.com>
2024-03-27 13:30:13 -0400

    Add script and function to allow one card only to be selected AND get the price of the card and set it in the form

--- public/js/clerk-book-room-script.js
+++ public/js/clerk-book-room-script.js
@@ -2,11 +2,13 @@ $(document).ready(() => {
 
     // ----------------- CARDS Animations -------------------
 
+
     // This variable prevents that when the card is first clicked
     // that the card add the class clicked and removes it in the 
     // next line
     let noFirstClick = false;
-    $('.card').click(function() {
+    let enabledCardPrice = 0;      
+    function cardHoverEffect() {
         let $this = $(this);
         $(this).children('.icon-container').toggleClass('hide');
 
@@ -14,6 +16,7 @@ $(document).ready(() => {
         if(!$(this).hasClass('clicked')) {
             $(this).removeClass('not-clicked');
             $(this).addClass('clicked');
+            disableOtherCards($this);
         }
 
         // when the card was in a clicked state
@@ -23,6 +26,7 @@ $(document).ready(() => {
         if($(this).hasClass('clicked') && noFirstClick) {
             $(this).removeClass('clicked');
             $(this).addClass('fades-out');
+            enableOtherCards($this);
             
             setTimeout(function(){
                 $this.addClass('not-clicked');
@@ -36,8 +40,61 @@ $(document).ready(() => {
         // If the first click happened we inverts the value of the no first click
         // The inverts is IMPORTANT 
         // Only God knows why 
-        noFirstClick = !noFirstClick;   
-    });
+        noFirstClick = !noFirstClick;
+    }
+
+    // Adds card hover effect to all cards
+    $('.card').click(cardHoverEffect);
+
+    function disableOtherCards(cardClicked) {
+        let priceString = $(cardClicked).find('.price-digit').text();
+        enabledCardPrice = parseInt(priceString);
+        //set the price of the hidden input
+        $('#room-price').val(enabledCardPrice);
+
+        $('.card-room').not(cardClicked).each(function() {
+            $(this).addClass('disabled');
+            $(this).removeClass('not-clicked');
+            $(this).off('click');
+            $(this).find('.card-title').addClass('disabled');
+            $(this).find('.price').addClass('disabled');
+        });
+    }
+
+    function enableOtherCards(cardClicked) {
+        enabledCardPrice = 0;
+        //set the price of the hidden input
+        $('#room-price').val(enabledCardPrice);
+
+        $('.card-room').not(cardClicked).each(function() {
+            $(this).removeClass('disabled');
+           $(this).addClass('not-clicked');
+           $(this).click(cardHoverEffect);
+           $(this).find('.card-title').removeClass('disabled');
+           $(this).find('.price').removeClass('disabled');
+           noFirstClick = false;
+        });
+    }
+    function disableAllCards() {
+        $('.card-room').each(function() {
+           $(this).addClass('disabled');
+           $(this).removeClass('not-clicked');
+           $(this).off('click');
+           $(this).find('.card-title').addClass('disabled');
+           $(this).find('.price').addClass('disabled');
+           
+        });
+    }
+    function enableAllCards() {
+        $('.card-room').each(function() {
+           $(this).removeClass('disabled');
+           $(this).addClass('not-clicked');
+           $(this).click(cardHoverEffect);
+           $(this).find('.card-title').removeClass('disabled');
+           $(this).find('.price').removeClass('disabled');
+           noFirstClick = false;
+        });
+    }
 
     // ----------------- CARDS Animations -------------------
     
@@ -46,5 +103,7 @@ $(document).ready(() => {
 
     var currentDate = new Date().toISOString().split('T')[0];
     $('#arrival-date')[0].value = currentDate;
+
+    // ----------------- Current date for arrival Date Picker -------------------
     
 });
